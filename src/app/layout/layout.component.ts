c7fb3ba9b4c68403415c79f59d8d6433b39718a2
swe <dimitrios.giannopoulos@gmail.com>
2019-04-15 17:38:40 +0200

    toggling sidenav

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  /**
+   * sidenavToggle
+   */
+  public sidenavToggle() {
+    alert('click');
   }
-
+  ngOnInit() {}
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [CommonModule],
-  exports: [],
+  imports: [CommonModule, MatSidenavModule, MatButtonModule],
+  exports: [MatSidenavModule, MatButtonModule],
   declarations: []
 })
 export class MaterialModule {}
