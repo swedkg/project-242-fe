ba0bc5edcfce3dedd3b45229ae502d15eb882ca1
Dimitrios Giannopoulos <dimitrios.giannopoulos@gmail.com>
2020-05-06 20:43:45 +0200

    notifications go to message

--- src/app/components/notifications/notifications.component.ts
+++ src/app/components/notifications/notifications.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { Message } from "../../_models/message.model";
 import { NotificationsService } from "../../_services/notifications.service";
+import { UserService } from "../../_services/user.service";
+import { User } from "../../_models/user";
+import { SidenavService } from "../../_services/sidenav.service";
+import { Store } from "@ngrx/store";
+import * as fromStore from "../../store";
 
 @Component({
   selector: "app-notifications",
@@ -9,14 +16,47 @@ import { NotificationsService } from "../../_services/notifications.service";
 export class NotificationsComponent implements OnInit {
   matBadgeHiddenAllNots: boolean = true;
   notificationsAllBadge: string;
+  current_user: User;
+  notifications$: Observable<Message[]>;
 
-  constructor(private NotificationsService: NotificationsService) {}
+  goToMessages(id) {
+    let req;
+    this.store
+      .select(fromStore.getSingleRequest, id)
+      .subscribe((request: any) => {
+        req = request[0];
+      });
+    let tab =
+      req.owner_id === this.current_user.id
+        ? this.SidenavService.tabs.myRequests
+        : this.SidenavService.tabs.myResponses;
+    this.SidenavService.setActiveSidenavTab(tab);
+    this.SidenavService.setExpandedAccordionPanel(id);
+    console.log(id, req, req.owner_id === this.current_user.id);
+  }
+
+  constructor(
+    private NotificationsService: NotificationsService,
+    private UserService: UserService,
+    private SidenavService: SidenavService,
+    private store: Store<fromStore.PlatformState>
+  ) {}
 
   ngOnInit() {
-    this.NotificationsService.getNotitifications().subscribe((data) => {
+    this.UserService.currentUserSubject.subscribe((data) => {
+      if (this.UserService.isLoggedIn) {
+        this.current_user = this.UserService.currentUserDetails;
+      }
+    });
+
+    this.notifications$ = this.NotificationsService.getNotitifications();
+
+    this.notifications$.subscribe((data) => {
       this.notificationsAllBadge = String(data.length);
-      this.matBadgeHiddenAllNots = data.length == 0 ? true : false;
-      console.log(data, this.notificationsAllBadge); //, this.matBadgeHiddenAllNots);
+      data.forEach((d) => {
+        console.log(d);
+      });
+      console.log(data, this.notificationsAllBadge);
     });
   }
 }
