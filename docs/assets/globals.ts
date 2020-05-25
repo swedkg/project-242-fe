3e5ecdd38d5a4c8db7686e0a645d8c7639718ff2
Dimitrios Giannopoulos <dimitrios.giannopoulos@gmail.com>
2020-05-25 20:11:59 +0200

    gh pages build

---
+++ docs/assets/globals.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+// @Injectable()
+export const Globals = Object.freeze({
+  id: 1,
+  email: 'user@example.com',
+  created_at: '2019-09-06 19:12:11',
+  updated_at: '2019-09-17 17:17:33',
+  authentication_token: '99B5FkfAFeeauq6oPaaz',
+  firstName: 'John',
+  lastName: 'Doe'
+});
